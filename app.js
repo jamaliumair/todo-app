cb7809fb4af2f557e258aac185f9e10fb4c18aa5
jamaliumair <jamaliumair957@gmail.com>
2024-08-18 15:36:09 +0500

    mark task as completed added

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ let addbutton = document.querySelectorAll(".add-btn");
 
 let allTodos = []
 let editindex = -1;
+let completedTodos = [];
 
 function add() {
     
     let newTodo = todoInput[0].value;
     if (editindex === -1 && newTodo !== "") {
         allTodos.push(newTodo);
+        completedTodos.push(false);
 
     } else {
 
@@ -30,14 +32,21 @@ function printAlltodos() {
     console.log(allTodos);
     divEl[0].innerHTML = "";
     for (let i = 0; i < allTodos.length; i++) {
-        
+
         divEl[0].innerHTML += `
-        <p id="todo-${i}">${allTodos[i]}
-        <span class="btns">
-        <button onclick="edit(${i})">Edit</button>
-        <button onclick="deleted(${i})">Delete</button>
-        </span>
+        <div class="todoContainer">
+
+        <p id="todo-${i}">
+        ${allTodos[i]}
         </p>
+
+        <div class="btns">
+        <button onclick="edit(${i})" class="hide${i}">Edit</button>
+        <button onclick="deleted(${i})">Delete</button>
+        <button onclick="completeTodo(${i})">Mark as Completed</button>
+        </div>
+
+        </div>
         `;
 
         
@@ -58,6 +67,28 @@ function deleted(index) {
 
     allTodos.splice(index,1);
     // console.log(allTodos);
+    completedTodos.splice(index,1);
     printAlltodos();
 }
 
+
+function completeTodo(index) {
+    completedTodos[index] = !completedTodos[index];
+
+    // Select the correct button by using the dynamic class name
+    let editButton = document.querySelector(`.hide${index}`);
+
+    if (completedTodos[index]) {
+        editButton.style.display = "none";
+    } else {
+        editButton.style.display = "inline-block";
+    }
+
+    // Add a class to mark the item as completed (optional)
+    let todoText = document.querySelector(`#todo-${index}`);
+    if (completedTodos[index]) {
+        todoText.classList.add('completed');
+    } else {
+        todoText.classList.remove('completed');
+    }
+}
